d6376b4e55ce9e135364545a8fb889e3cf50bcd5
chiefkana <kanat.bektursyn@gmail.com>
2025-01-29 23:07:35 +0500

    feat: added theme

---
+++ astro/src/types.ts
@@ -0,0 +1,29 @@
+import type socialIcons from "@assets/socialIcons";
+
+export type Site = {
+  website: string;
+  author: string;
+  profile: string;
+  desc: string;
+  title: string;
+  ogImage?: string;
+  lightAndDarkMode: boolean;
+  productPerIndex: number;
+  productPerPage: number;
+  postPerIndex: number;
+  postPerPage: number;
+  scheduledPostMargin: number;
+  showArchives?: boolean;
+  editPost?: {
+    url?: URL["href"];
+    text?: string;
+    appendFilePath?: boolean;
+  };
+};
+
+export type SocialObjects = {
+  name: keyof typeof socialIcons;
+  href: string;
+  active: boolean;
+  linkTitle: string;
+}[];
